8bf7c3e64936d4ec0b48da4cb9c8bb52e9c4e7ae
rodrigosecko <rodrigosecko@gmail.com>
2020-02-27 22:54:27 -0400

    partials views

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,7 +1,17 @@
 const controller = {};
 
-controller.list = (req,res) =>{
-    res.send('hola mundo!!');
+controller.list = (req, res) =>{
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer',(err, customers) => {
+            if(err){
+                res.json(err);//en caso de error
+            }
+            console.log(customers);
+            res.render('customers',{
+                data: customers
+            })
+        });
+    });
 };
 
 module.exports = controller;
